37eb3c50475b62b224ddd283a7f4de64d956b131
agconti <agc11d@gmail.com>
2015-12-30 20:39:15 -0500

    feat(): initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# angular2-quickstart
+The hello world starter project from angular.io's quickstart
---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core'
+
+@Component({
+  selector: 'my-app'
+, template: '<h1>Hello angular 2 world!</h1>'
+})
+export class AppComponent {}
