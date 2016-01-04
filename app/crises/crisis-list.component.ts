92aff9a79932abe80b8903d73a6243ba8a101860
agconti <agc11d@gmail.com>
2016-01-03 21:37:53 -0500

    feat(crises/crisis-list.component): displayed top crises

--- app/crises/crisis-list.component.ts
+++ app/crises/crisis-list.component.ts
@@ -1,12 +1,33 @@
-import {Component} from 'angular2/core'
+import {Component, OnInit} from 'angular2/core'
+import {Crisis} from './ICrisis'
 import {CrisisDetailComponent} from './crisis-detail.component'
+import {CrisisService} from './crisis.service'
+
 
 @Component({
   selector: 'crisis-list'
+, providers: [CrisisService]
+, directives: [CrisisDetailComponent]
 , template: `
-    <p>So <b>much</b> crisis!</p>
+    <h2>Today's Top Crises</h2>
+    <ul>
+      <li *ngFor="#crisis of crises">
+        <span class="badge">{{crisis.id}}</span> {{crisis.description}}
+      </li>
+    </ul>
     <crisis-detail></crisis-detail>
   `
-, directives: [CrisisDetailComponent]
 })
-export class CrisisListComponent {}
+export class CrisisListComponent implements OnInit {
+  public crises: Crisis[]
+
+  constructor(private _crisisService: CrisisService) {}
+
+  ngOnInit() {
+    this.getCrises()
+  }
+
+  getCrises() {
+    this._crisisService.getCrises().then(crises => this.crises = crises)
+  }
+}
