6b6c160132e633b6a501f045808104dbf05ff32f
agconti <agc11d@gmail.com>
2016-01-03 21:14:15 -0500

    feat(crises/crisis-detail.component): basic implementation of detail view

---
+++ app/crises/crisis-detail.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core'
+
+@Component({
+  selector: 'crisis-detail'
+, template: `<p>Detail!</p>`
+})
+export class CrisisDetailComponent {}
--- app/crises/crisis-list.component.ts
+++ app/crises/crisis-list.component.ts
@@ -1,7 +1,12 @@
 import {Component} from 'angular2/core'
+import {CrisisDetailComponent} from './crisis-detail.component'
 
 @Component({
   selector: 'crisis-list'
-, template: `<p>So <b>much</b> crisis!</p>`
+, template: `
+    <p>So <b>much</b> crisis!</p>
+    <crisis-detail></crisis-detail>
+  `
+, directives: [CrisisDetailComponent]
 })
 export class CrisisListComponent {}
