549179ecb079948e8285f58f06109c918a7c4b8a
agconti <agc11d@gmail.com>
2016-01-03 21:41:08 -0500

    feat(crises/crisis-list.component): added basic styling

--- app/crises/crisis-list.component.ts
+++ app/crises/crisis-list.component.ts
@@ -10,13 +10,61 @@ import {CrisisService} from './crisis.service'
 , directives: [CrisisDetailComponent]
 , template: `
     <h2>Today's Top Crises</h2>
-    <ul>
+    <ul class="crises">
       <li *ngFor="#crisis of crises">
         <span class="badge">{{crisis.id}}</span> {{crisis.description}}
       </li>
     </ul>
     <crisis-detail></crisis-detail>
   `
+, styles:[`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .crises {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 20em;
+    }
+    .crises li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0em;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .crises li.selected:hover {
+      color: white;
+    }
+    .crises li:hover {
+      color: #607D8B;
+      background-color: #EEE;
+      left: .1em;
+    }
+    .crises .text {
+      position: relative;
+      top: -3px;
+    }
+    .crises .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0em 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0px 0px 4px;
+    }
+  `]
 })
 export class CrisisListComponent implements OnInit {
   public crises: Crisis[]
