3ff8349624e188ce040c6c6f2910f6e2b6712091
agconti <agc11d@gmail.com>
2016-01-10 13:28:03 -0500

    feat(app.component): updated hero routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,21 @@
 import {Component} from 'angular2/core'
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router'
-import {CrisisListComponent} from './crises/crisis-list.component'
-import {HeroListComponent} from './heroes/hero-list.component'
+import {CrisisCenterComponent} from './crises/crisis-center.component'
+import {HeroComponent} from './heroes/hero.component.ts'
 
 
 @Component({
   selector: 'my-app'
+, directives: [ROUTER_DIRECTIVES]
 , template: `
     <h1>Component Router</h1>
     <a [routerLink]="['CrisisCenter']">Crisis Center</a>
     <a [routerLink]="['Heroes']">Heroes</a>
     <router-outlet></router-outlet>
   `
-, directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-  {path:'/crisis-center', name: 'CrisisCenter', component: CrisisListComponent}
-, {path:'/heroes', name: 'Heroes', component: HeroListComponent}
+  {path:'/crisis-center/...', name: 'CrisisCenter', component: CrisisCenterComponent, useAsDefault: true}
+, {path:'/heroes/...', name: 'Heroes', component: HeroComponent}
 ])
 export class AppComponent { }
